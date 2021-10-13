561c7ac2050e89574f5e3a2a636325b7a6dccee7
Dáša Pawlasová <brigitka.prch@gmail.com>
2021-10-13 14:15:32 +0200

    Basic implementation of matrix

--- src/data_structures/matrix.h
+++ src/data_structures/matrix.h
@@ -1,14 +1,63 @@
 //
-// Created by Dominik Klement on 12/10/2021.
+// Created by Dáša Pawlasová on 12.10.2021.
 //
+
+#include <vector>
+#include <iomanip>
 
 #ifndef FEEDFORWARDNEURALNET_MATRIX_H
 #define FEEDFORWARDNEURALNET_MATRIX_H
+#define ELEMENT_TYPE double
+#define MATRIX_INITIAL 0
+#define DECIMAL_PLACES_IN_PRINT 2
 
+using namespace std;
 
-class matrix {
+/**
+ * Class representing a matrix
+ */
+class Matrix {
+private:
+    vector <vector<ELEMENT_TYPE>> matrix;
+    int rows;
+    int cols;
+public:
 
+    /**
+     * Matrix class constructor, initiates the matrix with zeros
+     * @param rows - amount of rows in the matrix
+     * @param cols - amount of columns in the matrix
+     */
+    Matrix(int rows, int cols);
+
+    /**
+     * Prints matrix to standard output
+     */
+    void PrintMatrix();
+
+    /**
+     * Get vectors of the matrix
+     * @return Vectors of the matrix
+     */
+    vector <vector<ELEMENT_TYPE>> GetMatrix(){
+        return matrix;
+    }
+
+    /**
+     * Gets amount of rows in matrix.
+     * @return amount of rows
+     */
+    int GetRows(){
+        return rows;
+    }
+
+    /**
+     * Gets amount of columns in matrix.
+     * @return amount of columns
+     */
+    int GetCols(){
+        return cols;
+    }
 };
-
 
 #endif //FEEDFORWARDNEURALNET_MATRIX_H
